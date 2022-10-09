e744ff8bc13be378927b979d5fdfb3b63a703947
JeevanIITH <80705736+JeevanIITH@users.noreply.github.com>
2022-10-10 00:45:12 +0530

    updates

--- Semantics/symbol_table.h
+++ Semantics/symbol_table.h
@@ -9,6 +9,8 @@
 
 enum data_type{ int_t , double_t , float_t , char_t ,array_t};
 
+
+// only one data type can exist for a variable ,so respective value 
 union Value
 {
     int int_value;
@@ -18,45 +20,52 @@ union Value
 }typedef value_t;
 
 
-union array_info
+
+//for array data_type , array_size
+struct array_data
 {
     enum data_type data_type_t;
     unsigned int array_size;
-}typedef array_info_t;
+}typedef array_data_t;
 
-union function_info
+// function data , return type ,parameters data_type list
+struct function_data
 {
+    enum data_type return_data_type;
+    int no_parameters;
+    // paramter list ......
     
-}typedef fun_info;
+}typedef fun_data_t;
 
 
 
 union identifier_data
 {
     //array information array size , data type
-    array_info_t array_inf;
+    array_data_t array_info;
 
     //return type for function
-    enum data_type return_type;
-}typedef id_data;
+    fun_data_t function_info;
+}typedef id_data_t;
 
 
 struct item_
 {
-    char* name;
 
-    value_t value_;
+    char* name;              // name of the identifier
+    enum data_type data_type_t;    // data type of the variable
+    value_t value_;               // value of the variable
 
-    int scope;
+    int scope;                      // scope of the variable
 
-    enum data_type data_type_t;
-    id_data data_info;
+    /*if the identifier is function , array  then id_data is used for extra information*/  
+    id_data_t data_info;            
 
 
 
 
     struct item_* next;
-};
+}typedef item_t;
 
 
 
@@ -66,6 +75,6 @@ void init_hash();
 
 unsigned int hash_key();
 
-void insert(char* name,   value_t val , );
+void insert(char* name,enum data_type data_type_t , value_t val ,int scope , id_data_t data_info );
 
 
