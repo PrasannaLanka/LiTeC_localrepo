33b8c6c891bcedde2bca6f47f9d41c3613eaef84
JeevanIITH <80705736+JeevanIITH@users.noreply.github.com>
2022-10-10 07:32:11 +0530

    update

--- Semantics/symbol_table.c
+++ Semantics/symbol_table.c
@@ -7,12 +7,13 @@
 
 void init_hash()
 {
+    symbol_table_t=(item_t**)malloc(sizeof(item_t*)*MAX_SYMBOL_TABLE_SIZE);
 
     
 }
 
 //using identifier name , generate an unsigned key 
-unsigned int hash_key(char id_name[],int len)
+unsigned int hash_key(char *id_name,int len)
 {
     unsigned int key;
 
@@ -21,12 +22,18 @@ unsigned int hash_key(char id_name[],int len)
         key+=(int)id_name[i]-(int)id_name/2;
     }
     key+=len+(int)id_name[len/2];
-    return key%MAX_HASH_SIZE;
+    return key%MAX_SYMBOL_TABLE_SIZE;
 }
 
 
 unsigned int insert_symbol_tbl(char* name,enum data_type data_type_t , value_t val ,int scope , id_data_t data_info )
 {
+    size_t sz=strlen(name);
+    int l = (int)sz;
+    
+    unsigned int key=hash_key(name,l);
+    item_t head;
+    head=symbol_table_t[1];
 
 
 
