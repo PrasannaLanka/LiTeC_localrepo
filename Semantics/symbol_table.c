16307a1382e12ee9cc0ef060ba394091326e5d11
JeevanIITH <80705736+JeevanIITH@users.noreply.github.com>
2022-10-10 09:00:17 +0530

    update

--- Semantics/symbol_table.c
+++ Semantics/symbol_table.c
@@ -4,12 +4,10 @@
 #include <string.h>
 
 
-
+static item_t **symbol_table_t;
 void init_hash()
 {
     symbol_table_t=(item_t**)malloc(sizeof(item_t*)*MAX_SYMBOL_TABLE_SIZE);
-
-    
 }
 
 //using identifier name , generate an unsigned key 
@@ -28,12 +26,40 @@ unsigned int hash_key(char *id_name,int len)
 
 unsigned int insert_symbol_tbl(char* name,enum data_type data_type_t , value_t val ,int scope , id_data_t data_info )
 {
+
     size_t sz=strlen(name);
     int l = (int)sz;
     
     unsigned int key=hash_key(name,l);
-    item_t head;
+    item_t *head;
     head=symbol_table_t[1];
+    item_t *it=head;
+    while (it!=NULL)
+    {
+        //if already declared and exist in symbol table
+        if (it->name==name)
+        {
+            /* give error */
+            break;
+        }
+
+
+        
+        it=it->next;
+    }
+    if(it==NULL)
+    {
+        item_t *new_item=(item_t*)malloc(sizeof(item_t*));
+        new_item->name=name;
+        new_item->data_type_t=data_type_t;
+        new_item->value_=val;
+        new_item->scope=scope;
+        new_item->data_info;
+        it=new_item;
+        
+    }
+    
+    
 
 
 
