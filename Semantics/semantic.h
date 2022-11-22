865fa00e612e444add9bbc88b059726b553befca
Sandeep-L <80530377+Sandeep-L@users.noreply.github.com>
2022-11-22 17:18:22 +0530

    Update semantic.h

--- Semantics/semantic.h
+++ Semantics/semantic.h
@@ -1,6 +1,7 @@
 #include "ast.h"
 void check_function_return(ast_node *node);
 void check_assignment(symbol_table* sym_tbl , ast_node *node);
+void check_declaration(ast_node *node);
 void check_function_parameters(param* par,ast_node* node);
 
 int check_matrix(ast_node *parent ,ast_node *left_node , ast_node *right_node);
