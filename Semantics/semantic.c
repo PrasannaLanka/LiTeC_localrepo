d323eed7098460848c25db1670f4fb0fef1e7903
Sandeep-L <80530377+Sandeep-L@users.noreply.github.com>
2022-11-22 17:22:03 +0530

    Scalar-Matrix/Matrix-Matrix multiplication, Type-checking and Matrix-checking

--- Semantics/semantic.c
+++ Semantics/semantic.c
@@ -67,6 +67,49 @@ void check_assignment(symbol_table *sym_tbl, ast_node *node)
     enum data_type_t data_type = item->data_type;
     if (node->right->data_type == data_type)
     {
+        if(node->right->isMatrix == 1 && item->iden_type == matrix_t)
+        {
+            node->isMatrix = 1;
+            return;
+        }
+        else if(node->right->isMatrix == 1 && item->iden_type != matrix_t)
+        {
+            printf("ERROR: Matrix cannot be assigned to a Scalar\n");
+            return;
+        }
+        else if(node->right->isMatrix != 1 && item->iden_type == matrix_t)
+        {
+            printf("ERROR: Scalar cannot be assigned to a Matrix\n");
+            return;
+        }
+        return;
+    }
+    else
+    {
+        printf("ERROR: Type mismatch at line %d\n", line_number);
+        return;
+    }
+}
+
+void check_declaration(ast_node *node)
+{
+    if(node->right->data_type == node->left->data_type)
+    {
+        if(node->right->isMatrix == 1 && node->left->isMatrix == 1)
+        {
+            node->isMatrix = 1;
+            return;
+        }
+        else if(node->right->isMatrix == 1 && node->left->isMatrix == 0)
+        {
+            printf("ERROR: Matrix cannot be assigned to a Scalar\n");
+            return;
+        }
+        else if(node->right->isMatrix == 0 && node->left->isMatrix == 1)
+        {
+            printf("ERROR: Scalar cannot be assigned to a Matrix\n");
+            return;
+        }
         return;
     }
     else
@@ -144,36 +187,130 @@ void check_function_parameters(param *par, ast_node *node)
 }
 
 int check_matrix(ast_node *parent , ast_node *left_node , ast_node *right_node){
-    if (left_node->isMatrix!=1 || right_node->isMatrix!=1)
+    if(left_node->isMatrix == 1 && right_node->isMatrix == 0)
     {
-        printf("ERROR : both are not matrix \n ");
-        return 1;
-    }
-    char *operator;
-    operator="+";
-    if (strcmp(parent->name,operator)==0)
-    {
-        if (left_node->matrix_info.row!=right_node->matrix_info.row)
+        char *operator_mul;
+        operator_mul="*";
+        if(strcmp(parent->name, operator_mul) == 0)
         {
-            printf("ERROR: no of rows didn't matched \n ");
-            return 1;
-        }
-        if (left_node->matrix_info.column!=right_node->matrix_info.column)
-        {
-            printf("ERROR: no of columns didn't matched \n ");
-            return 1;
-        }
-        else
-        {
-            parent->isMatrix=1;
-            parent->matrix_info.row=left_node->matrix_info.row;
-            parent->matrix_info.column=left_node->matrix_info.column;
-            printf("Both matrix matched \n ");
+            parent->isMatrix = 1;
+            parent->matrix_info.row = left_node->matrix_info.row;
+            parent->matrix_info.column = left_node->matrix_info.column;
+            printf("Scalar Multiplication with left variable as the matrix");
             return 0;
         }
-        
+
+        char *operator_div;
+        operator_div="/";
+        if(strcmp(parent->name, operator_div) == 0)
+        {
+            parent->isMatrix = 1;
+            parent->matrix_info.row = left_node->matrix_info.row;
+            parent->matrix_info.column = left_node->matrix_info.column;
+            printf("Scalar Division with left variable as the matrix");
+            return 0;
+        }
+
+        printf("ERROR: Only Scalar Multiplication and Division are possible\n");
+        return 1;
     }
+    else if(left_node->isMatrix == 0 && right_node->isMatrix == 1)
+    {
+        char *operator_mul;
+        operator_mul="*";
+        if(strcmp(parent->name, operator_mul) == 0)
+        {
+            parent->isMatrix = 1;
+            parent->matrix_info.row = right_node->matrix_info.row;
+            parent->matrix_info.column = right_node->matrix_info.column;
+            printf("Scalar Multiplication with right variable as the matrix");
+            return 0;
+        }
+
+        char *operator_div;
+        operator_div="/";
+        if(strcmp(parent->name, operator_div) == 0)
+        {
+            printf("ERROR: Scalar divided by a Matrix is Impossible\n");
+            return 1;
+        }
+
+        printf("Only Scalar Multiplication and Division are possible\n");
+        return 1;
+    }
+    else
+    {
+        char *operator_add;
+        operator_add="+";
+        if (strcmp(parent->name,operator_add)==0)
+        {
+            if (left_node->matrix_info.row!=right_node->matrix_info.row)
+            {
+                printf("ERROR: no of rows didn't matched \n ");
+                return 1;
+            }
+            if (left_node->matrix_info.column!=right_node->matrix_info.column)
+            {
+                printf("ERROR: no of columns didn't matched \n ");
+                return 1;
+            }
+            else
+            {
+                parent->isMatrix=1;
+                parent->matrix_info.row=left_node->matrix_info.row;
+                parent->matrix_info.column=left_node->matrix_info.column;
+                printf("Both matrix matched \n ");
+                return 0;
+            }
+            
+        }
+
+        char *operator_sub;
+        operator_add = "-";
+        if(strcmp(parent->name, operator_add) == 0)
+        {
+            if (left_node->matrix_info.row != right_node->matrix_info.row)
+            {
+                printf("ERROR: No of rows didn't matched \n ");
+                return 1;
+            }
+            if (left_node->matrix_info.column != right_node->matrix_info.column)
+            {
+                printf("ERROR: No of columns didn't matched \n ");
+                return 1;
+            }
+            else
+            {
+                parent->isMatrix = 1;
+                parent->matrix_info.row = left_node->matrix_info.row;
+                parent->matrix_info.column = left_node->matrix_info.column;
+                printf("Both matrix matched \n ");
+                return 0;
+            }
+            
+        }
+
+        char *operator_mul;
+        operator_mul = "*";
+        if(strcmp(parent->name, operator_mul) == 0)
+        {
+            if(left_node->matrix_info.column != right_node->matrix_info.row)
+            {
+                printf("ERROR: No of columns in the left matrix didn't match with the No of rows in the right matrix\n");
+                return 1;
+            }
+            else
+            {
+                parent->isMatrix = 1;
+                parent->matrix_info.row = left_node->matrix_info.row;
+                parent->matrix_info.column = right_node->matrix_info.column;
+                printf("The two matrices can be multiplied\n");
+                return 0;
+            }
+        }
+    }
+
+    printf("ERROR: Only Matrix Addition, Subtraction and Division are possible\n");
     return 1;
-    
-    
 }
+
